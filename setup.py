dd77c7512f041555649b5da3561c6d9d87ea4fe1
Leonardo Boiko <leoboiko@gmail.com>
2013-02-18 13:40:43 -0300

    add pypi-style classifiers

--- setup.py
+++ setup.py
@@ -40,5 +40,19 @@ setup(name='myougiden',
       scripts=['bin/myougiden', 'bin/updatedb-myougiden'],
       cmdclass=dict(install=updatedb_install),
       license='GPLv3',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Education',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Natural Language :: English',
+          'Natural Language :: Japanese',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3',
+          'Topic :: Education',
+          'Topic :: Text Processing :: Linguistic',
+          ]
      )
 
