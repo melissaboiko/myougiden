37abadf28921ed97089ba35a87dc6cef77988a9a
Leonardo Boiko <leoboiko@namakajiri.net>
2013-02-19 14:12:31 -0300

    guess next version

--- bin/release.sh
+++ bin/release.sh
@@ -10,16 +10,33 @@ fi
 function echo_do()
 {
   echo '***' "$@"
-  $*
+  "$@"
+}
+
+function next_version()
+{
+  if echo "$1" | grep -q "dev$"; then
+    newversion="$(echo "$1" | sed -e "s/dev$//")"
+  else
+    last="$(echo "$1" | sed -e "s/.*\([0-9]\+\)$/\1/")"
+    newversion="$(echo "$1" | sed -e "s/${last}$/$(($last + 1))/")"
+  fi
+  echo "$newversion"
 }
 
 set -e
 cd $(dirname "$0")/..
-version="$(./setup.py --version | sed -e "s/dev$//")"
-[ "$version" ] || ( echo "Could not find version=." ; exit 1 )
 
-echo_do sed -i setup.py -e "s/^version=.*/version='$version'/"
-echo_do git commit -a -m "releasing $version"
-echo_do git tag -u "$key" "$version" -m "releasing $version"
+if [ "$1" ]; then
+  newversion="$1"
+else
+  oldversion="$(./setup.py --version)"
+  [ "$oldversion" ] || ( echo "Could not find version=." ; exit 1 )
+  newversion="$(next_version $oldversion)"
+fi
+
+echo_do sed -i setup.py -e "s/^version=.*/version='$newversion'/"
+echo_do git commit -a -m "releasing $newversion"
+echo_do git tag -u "$key" "$newversion" -m "releasing $newversion"
 echo_do git push
 echo_do python3 setup.py sdist upload --sign --identity="$key"
