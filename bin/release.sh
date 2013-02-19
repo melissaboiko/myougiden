effcd110c4a81d7455cb780d6ad49cfe9819f326
Leonardo Boiko <leoboiko@gmail.com>
2013-02-18 23:02:20 -0300

    commit before (be careful!)

--- bin/release.sh
+++ bin/release.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# usage:
+# 1. update version in setup.py
+# 2. call this script (before git-commiting)
 key=leoboiko@namakajiri.net
 
 cd $(dirname "$0")
 version="$(python3 setup.py --version)"
+git commit -a -m "releasing $version"
 git tag -u "$key" "$version" -m "releasing $version"
 git push
 python3 setup.py sdist upload --sign --identity="$key"
