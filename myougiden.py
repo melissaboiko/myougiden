fdde48ab758c03e96985c793f551b190445deeef
Leonardo Boiko <leoboiko@gmail.com>
2013-02-18 22:05:56 -0300

    working match coloring

--- myougiden.py
+++ myougiden.py
@@ -21,7 +21,7 @@ def has_alpha(string):
     return re.search('[a-z]', string, re.I) is not None
 
 regexp_store = {}
-def get_regex(pattern, flags):
+def get_regexp(pattern, flags):
     '''Return a compiled regexp from persistent store; make one if needed.
 
     We use this helper function so that the SQL hooks don't have to
@@ -41,22 +41,22 @@ def get_regex(pattern, flags):
 
 def regexp_sensitive(pattern, field):
     '''SQL hook function for case-sensitive regexp matching.'''
-    reg = get_regex(pattern, 0)
+    reg = get_regexp(pattern, 0)
     return reg.search(field) is not None
 
 def regexp_insensitive(pattern, field):
     '''SQL hook function for case-insensitive regexp matching.'''
-    reg = get_regex(pattern, re.I)
+    reg = get_regexp(pattern, re.I)
     return reg.search(field) is not None
 
 def match_word_sensitive(word, field):
     '''SQL hook function for whole-word, case-sensitive, non-regexp matching.'''
-    reg = get_regex(r'\b' + re.escape(word) + r'\b', 0)
+    reg = get_regexp(r'\b' + re.escape(word) + r'\b', 0)
     return reg.search(field) is not None
 
 def match_word_insensitive(word, field):
     '''SQL hook function for whole-word, case-sensitive, non-regexp matching.'''
-    reg = get_regex(r'\b' + re.escape(word) + r'\b', re.I)
+    reg = get_regexp(r'\b' + re.escape(word) + r'\b', re.I)
     return reg.search(field) is not None
 
 def testdb():
@@ -103,31 +103,83 @@ FORMATTING={
 def fmt(string, style):
     return colored(string, *(FORMATTING[style]))
 
+def color_regexp(reg_obj, longstring, base_style=None):
+    '''Search regexp in longstring; return longstring with match colored.'''
 
-def format_entry_tsv(kanjis, readings, senses, search_params=None, color=False):
-    if color:
-        return '%s\t%s\t%s' % (
-                fmt('；', 'subdue').join([fmt(r, 'reading') for r in readings]),
-                fmt('；', 'subdue').join([fmt(k, 'kanji') for k in kanjis]),
-                "\t".join([fmt('; ', 'subdue').join(glosses_list) for glosses_list in senses])
-                )
+    m = reg_obj.search(longstring)
+    if not m:
+        return longstring
     else:
-        return '%s\t%s\t%s' % (
-                '；'.join(readings),
-                '；'.join(kanjis),
-                "\t".join(['; '.join(glosses_list) for glosses_list in senses])
-                )
-def format_entry_human(kanjis, readings, senses, search_params=None, color=True):
+        head = longstring[:m.start()]
+        tail = longstring[m.end():]
+        if base_style:
+            head = fmt(head, base_style)
+            tail = fmt(tail, base_style)
+        return head + fmt(m.group(), 'match') + tail
 
+
+def colorize_data(kanjis, readings, senses, search_params):
+    '''Colorize matched data according to search parameters.
+
+    search_params: A dictionary of arguments like those of search_by().
+    '''
+
+    # TODO: there's some duplication between this logic and search_by()
+
+    # regexp to match whatever the query matched
+    reg = search_params['query']
+    if not search_params['regexp']:
+        reg = re.escape(reg)
+
+    if search_params['extent'] == 'whole':
+        reg = '^' + reg + '$'
+    elif search_params['extent'] == 'word':
+        reg = r'\b' + reg + r'\b'
+
+    if search_params['case_sensitive']:
+        reg = get_regexp(reg, 0)
+    else:
+        reg = get_regexp(reg, re.I)
+
+
+    if search_params['field'] == 'reading':
+        readings = [color_regexp(reg, r, 'reading') for r in readings]
+        kanjis = [fmt(k, 'kanji') for k in kanjis]
+    elif search_params['field'] == 'kanji':
+        readings = [fmt(k, 'reading') for k in readings]
+        kanjis = [color_regexp(reg, k, 'kanji') for k in kanjis]
+    elif search_params['field'] == 'gloss':
+        readings = [fmt(k, 'reading') for k in readings]
+        kanjis = [fmt(k, 'kanji') for k in kanjis]
+        senses = [[color_regexp(reg, g) for g in glosses_list]
+                  for glosses_list in senses]
+
+    return (kanjis, readings, senses)
+
+
+def format_entry_tsv(kanjis, readings, senses, search_params, color=False):
     sep_full = '；'
     sep_half = '; '
 
-    glosses = {}
     if color:
         sep_full = fmt(sep_full, 'subdue')
         sep_half = fmt(sep_half, 'subdue')
-        readings = [fmt(r, 'reading') for r in readings]
-        kanjis = [fmt(k, 'kanji') for k in kanjis]
+        kanjis, readings, senses = colorize_data(kanjis, readings, senses, search_params)
+
+    return '%s\t%s\t%s' % (
+                sep_full.join(readings),
+                sep_full.join(kanjis),
+                "\t".join([sep_half.join(glosses_list) for glosses_list in senses])
+                )
+
+def format_entry_human(kanjis, readings, senses, search_params, color=True):
+    sep_full = '；'
+    sep_half = '; '
+
+    if color:
+        sep_full = fmt(sep_full, 'subdue')
+        sep_half = fmt(sep_half, 'subdue')
+        kanjis, readings, senses = colorize_data(kanjis, readings, senses, search_params)
 
     s = ''
 
@@ -145,12 +197,10 @@ def format_entry_human(kanjis, readings, senses, search_params=None, color=True)
             sn = fmt(sn, 'misc')
         s += sn + ' '
 
-        # if color:
-        #     glosses_list = [fmt(g, 'gloss') for g in glosses_list]
-
         s += sep_half.join(glosses_list)
 
     return s
+
 
 def fetch_entry(cur, ent_seq):
     '''Return tuple of lists (kanjis, readings, senses).'''
@@ -181,7 +231,6 @@ def fetch_entry(cur, ent_seq):
         senses.append(glosses)
 
     return (kanjis, readings, senses)
-
 
 def search_by(cur, field, query, extent='whole', regexp=False, case_sensitive=False):
     '''Main search function.  Return list of ent_seqs.
@@ -272,5 +321,4 @@ def guess_search(cur, conditions):
         if len(res) > 0:
             return (condition, res)
     return []
-
 
