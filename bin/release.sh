28662b77765957b2a5698098356b1e98eaf1ceb0
Leonardo Boiko <leoboiko@gmail.com>
2013-02-18 23:07:30 -0300

    releasing 0.2

--- bin/release.sh
+++ bin/release.sh
@@ -2,8 +2,15 @@
 # usage:
 # 1. update version in setup.py
 # 2. call this script (before git-commiting)
-key=leoboiko@namakajiri.net
+key='<leoboiko@namakajiri.net>'
 
+if ! gpg --list-keys --with-colons | grep "^pub" | grep -q "$key:"
+then
+  echo "Could not find GPG key $key!"
+  exit 1
+fi
+
+set -e
 cd $(dirname "$0")/..
 version="$(python3 setup.py --version)"
 git commit -a -m "releasing $version"
