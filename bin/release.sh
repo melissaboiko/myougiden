d791d59742ae531f4d9e63ce8844df1c17420272
Leonardo Boiko <leoboiko@gmail.com>
2013-02-18 23:03:00 -0300

    fix cwd bug

--- bin/release.sh
+++ bin/release.sh
@@ -4,7 +4,7 @@
 # 2. call this script (before git-commiting)
 key=leoboiko@namakajiri.net
 
-cd $(dirname "$0")
+cd $(dirname "$0")/..
 version="$(python3 setup.py --version)"
 git commit -a -m "releasing $version"
 git tag -u "$key" "$version" -m "releasing $version"
