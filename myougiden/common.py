023827f47733f15d86dd6526f89283156e04f441
Leonardo Boiko <leoboiko@namakajiri.net>
2013-02-22 15:42:03 -0300

    --version

--- myougiden/common.py
+++ myougiden/common.py
@@ -320,3 +320,83 @@ def abbrevs_table(cur):
     for row in cur.fetchall():
         abbrevs.append(row[0])
     return "\n".join([abbrev_line(cur, abbrev) for abbrev in abbrevs])
+
+# credits: http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+def version(cur):
+    import argparse
+    import romkan
+    # import termcolor # no version
+    import platform
+
+    try:
+        import psutil
+        psutil_version = psutil.__version__
+    except ImportError:
+        psutil_version = None
+
+    cur.execute('''
+    SELECT dbversion, jmdict_mtime
+    FROM versions;
+    ''')
+    row = cur.fetchone()
+    dbversion = row[0]
+    jmdict_mtime = row[1]
+
+    scripts = {}
+    for s in ('gzip', 'rsync', 'nice', 'ionice'):
+        path = which(s)
+        if path:
+            scripts[s] = fmt(path, 'parameter')
+        else:
+            scripts[s] = fmt(path, 'warning')
+
+    return ('''
+myougiden version %s , database v. %s
+JMdict last modified %s
+Python version %s (%s)
+
+Libraries:
+  romkan: %s
+  argparse: %s
+  psutil: %s
+
+External programs:
+  gzip: %s
+  rsync: %s
+  nice: %s
+  ionice: %s
+'''.strip() % (
+    fmt(config['core']['version'], 'parameter'),
+    fmt(dbversion, 'parameter'),
+    fmt(jmdict_mtime, 'parameter'),
+    fmt(platform.python_version(), 'parameter'),
+    fmt(platform.platform(), 'parameter'),
+
+    fmt(romkan.__version__, 'parameter'),
+    # fmt(termcolor.__version__, 'parameter'),
+    fmt(argparse.__version__, 'parameter'),
+    fmt(psutil_version, 'parameter'),
+
+    scripts['gzip'],
+    scripts['rsync'],
+    scripts['nice'],
+    scripts['ionice'],
+    ))
