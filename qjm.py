1f23c17674dbf9cf5603eaab175efc8ac898b1f2
Leonardo Boiko <leoboiko@namakajiri.net>
2013-02-23 13:38:20 -0300

    convenience file

---
+++ qjm.py
@@ -0,0 +1,14 @@
+# convenience script for interactive use:
+# myougiden $ python3.2
+# >>> from qjm import *
+
+from myougiden import config
+from xml.etree.cElementTree import ElementTree as ET
+import gzip
+
+et = ET()
+jm = et.parse(gzip.open(config['paths']['jmdictgz'], 'r'))
+
+from xml.etree.cElementTree import tostring
+def tos(element):
+    tostring(element).decode()
