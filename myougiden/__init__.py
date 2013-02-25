baaf545db3542063ec258bffc426b4b2a93a165a
Leonardo Boiko <leoboiko@namakajiri.net>
2013-02-25 07:58:42 -0300

    detect if no config file

--- myougiden/__init__.py
+++ myougiden/__init__.py
@@ -8,13 +8,16 @@ def read_config():
 
     # default for running from source dir
     prefix = os.path.realpath(os.path.join(dirname, '..'))
-    config_path = 'etc/config.ini'
+    config_path = os.path.join(prefix, 'etc/config.ini')
 
     # detect installation prefix.
     # is there a better way of doing this?
     if re.search('/lib/', dirname):
         prefix = re.sub('/lib/.*', '', dirname)
-        config_path = 'etc/myougiden/config.ini'
+        config_path = os.path.join(prefix, 'etc/myougiden/config.ini')
+
+    if not os.path.isfile(config_path):
+        raise RuntimeError("ERROR: couldn't find config.ini at %s" % config_path)
 
     cp.read(os.path.join(prefix, config_path))
     cp.set('paths','prefix',prefix)
